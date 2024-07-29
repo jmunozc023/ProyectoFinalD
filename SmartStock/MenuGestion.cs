9f9c8c832c967d8db8cbacb73e381a444b47da03
jmunozc023 <jmunozc023@ulacit.ed.cr>
2024-07-29 04:31:49 -0600

    Update MenuGestion.cs

--- SmartStock/MenuGestion.cs
+++ SmartStock/MenuGestion.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace SmartStock
@@ -20,6 +21,7 @@ namespace SmartStock
             InitializeComponent();
             RellenarComboBox();
             RellenarComboBox1();
+            RellenarComboBox2();
         }
 
         protected override void OnFormClosing(FormClosingEventArgs e)
@@ -99,23 +101,47 @@ namespace SmartStock
                 MessageBox.Show("Error: " + ex.Message);
             }
         }
-       
-        
-        private void GestionAgrButton_Click(object sender, EventArgs e)
-        {
-            SqlConnection connection = new SqlConnection("Data Source=JOSE-LAPTOP\\MSSQLSERVER1;Initial Catalog=SmartStock;Trusted_Connection=True;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
-            string Nombre = GestionNombreBox.Text;
-            string Descripcion = GestionDescBox.Text;
-            string Marca = GestionMarcaBox.Text;
-            string Modelo = GestionModBox.Text;
-            DateTime Fecha = FechaGestion.Value;
-            string estado = GestionEstadoComboBox.SelectedIndex.ToString();
-            string categoria = GestionCatComboBox.SelectedIndex.ToString();
-            string subcategoria = GestionSubCatComboBox.SelectedIndex.ToString();
-            string imgLocation = CarImaPictureBox.ImageLocation;
-            
 
+    private byte[] GetImageData(string imagePath)
+    {
+        using (FileStream fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+        {
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                return br.ReadBytes((int)fs.Length);
+            }
         }
+    }
+
+    private void GestionAgrButton_Click(object sender, EventArgs e)
+    {
+        SqlConnection connection = new SqlConnection("Data Source=JOSE-LAPTOP\\MSSQLSERVER1;Initial Catalog=SmartStock;Trusted_Connection=True;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+        string Nombre = GestionNombreBox.Text;
+        string Descripcion = GestionDescBox.Text;
+        string Marca = GestionMarcaBox.Text;
+        string Modelo = GestionModBox.Text;
+        DateTime Fecha = FechaGestion.Value;
+        string estado = GestionEstadoComboBox.SelectedItem.ToString();
+        string categoria = GestionCatComboBox.SelectedIndex.ToString();
+        string subcategoria = GestionSubCatComboBox.SelectedIndex.ToString();
+        string imgLocation = CarImaPictureBox.ImageLocation;
+        byte[] imageData = GetImageData(imgLocation);
+        // Primero, insertamos los datos en la tabla Equipos
+        string query = "INSERT INTO Equipos (Nombre, Descripcion, Marca, Modelo, Fecha_ingreso, Estado, Categoria, Subcategoria, Imagen) OUTPUT INSERTED.ID_producto VALUES (@Nombre, @Descripcion, @Marca, @Modelo, @Fecha_ingreso, @Estado, @Categoria, @Subcategoria, @Imagen)";
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Nombre", Nombre);
+        command.Parameters.AddWithValue("@Descripcion", Descripcion);
+        command.Parameters.AddWithValue("@Marca", Marca);
+        command.Parameters.AddWithValue("@Modelo", Modelo);
+        command.Parameters.AddWithValue("@Fecha_ingreso", Fecha);
+        command.Parameters.AddWithValue("@Estado", estado);
+        command.Parameters.AddWithValue("@Categoria", categoria);
+        command.Parameters.AddWithValue("@Subcategoria", subcategoria);
+        command.Parameters.AddWithValue("@Imagen", imageData);
+        command.ExecuteNonQuery();
+        connection.Close();
+        MessageBox.Show("Equipo registrado exitosamente");
+    }
 
 
     }
