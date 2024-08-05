258a2b49092d7e998f94bd094fe649c2af3434e6
jmunozc023 <jmunozc023@ulacit.ed.cr>
2024-08-04 22:50:51 -0600

    Update LogicaConsulta.cs

--- Logica/LogicaConsulta.cs
+++ Logica/LogicaConsulta.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConexionSQL;
+using System.Data;
 
 namespace Logica
 {
-    internal class LogicaConsulta
+    public class LogicaConsulta
     {
+        private Consulta_BD consulta = new Consulta_BD();
+        public DataTable FiltrarPorIdYEstado(int idProducto, string estado)
+        {
+            DataTable dt = new DataTable();
+            dt = consulta.FiltrarPorIdYEstado(idProducto, estado);
+            return dt;
+        }
+
     }
 }
